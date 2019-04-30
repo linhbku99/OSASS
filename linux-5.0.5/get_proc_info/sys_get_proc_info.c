73aac4d7833281d350aa7481d37faf7665ac0bfa
Peter Linh <49335397+linhbku99@users.noreply.github.com>
2019-05-01 00:32:59 +0700

    Update sys_get_proc_info.c

--- linux-5.0.5/get_proc_info/sys_get_proc_info.c
+++ linux-5.0.5/get_proc_info/sys_get_proc_info.c
@@ -1,5 +1,6 @@
 #include <linux/kernel.h>
-#include <unistd.h>
+#include <linux/syscalls.h>
+//#include <unistd.h>
 struct proc_info {
 	pid_t pid;
 	char name[16];
@@ -13,12 +14,12 @@ struct procinfos {
 //asmlinkage long sys_get_proc_info(pid_t pid, struct procinfos *info)
 SYSCALL_DEFINE2(get_proc_info, pid_t, pid, struct procinfos *, info)
 {
-	info->studenID = 1710165;
-	printk("Student ID: %ld\n", info->studenID);
+	printk("Student ID: 1710165");
+	info->studentID = 1710165;
 	if (pid == -1) {
 		pid = current->pid;
 	}
-	struct tash_struct *proces;
+	struct task_struct *proces;
 	for_each_process (proces) {
 		if (proces->pid == pid) {
 			struct task_struct *cProces;
@@ -28,19 +29,24 @@ SYSCALL_DEFINE2(get_proc_info, pid_t, pid, struct procinfos *, info)
 			strcpy(info->proc.name, proces->comm);
 			if (proces->real_parent == NULL) {
 				info->parent_proc.pid = 0;
-				info->parent_proc.name = "\0";
+				//info->parent_proc.name = "\0";
+				strcpy(info->parent_proc.name, "\0");
 			} else {
 				info->parent_proc.pid =
 					proces->real_parent->pid;
-				info->patent_proc.name =
-					proces->real_parent->comm;
+				//info->patent_proc.name = proces->real_parent->comm;
+				strcpy(info->parent_proc.name,
+				       proces->real_parent->comm);
 			}
 			if (cProces == NULL) {
 				info->oldest_child_proc.pid = 0;
-				info->oldest_child_proc.name = "\0";
+				//info->oldest_child_proc.name = "\0";
+				strcpy(info->oldest_child_proc.name, "\0");
 			} else {
 				info->oldest_child_proc.pid = cProces->pid;
-				info->oldest_child_proc.name = cProces->comm;
+				//info->oldest_child_proc.name = cProces->comm;
+				strcpy(info->oldest_child_proc.name,
+				       cProces->comm);
 			}
 		}
 	}
